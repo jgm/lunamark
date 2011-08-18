4827303e8cf8421ef46231fdde75e4013523e1a3
John MacFarlane <fiddlosopher@gmail.com>
2011-08-18 08:52:14 -0700

    Started on html5 writer.

--- lunamark/writer/html5.lua
+++ lunamark/writer/html5.lua
@@ -1,7 +1,13 @@
 -- HTML5 writer for lunamark
 
-html = require("lunamark.writer.html")
+Html = require("lunamark.writer.html")
 
-Html5 = html
+Html5 = Html
+
+local format = Html.format
+
+function Html5.section(s,level,contents)
+  return format("\n<section>\n<h%d>%s</h%d>\n%s</section>\n",level,s,level,contents)
+end
 
 return Html5
