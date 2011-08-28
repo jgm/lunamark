6293ebda71023114156348a36391b7aeca71de6f
John MacFarlane <fiddlosopher@gmail.com>
2011-08-28 13:49:05 -0700

    More small changes to markdown_doclet.

--- luadoc/markdown_doclet.lua
+++ luadoc/markdown_doclet.lua
@@ -1,3 +1,18 @@
+--[[ Note!  This requires a slightly patched luadoc.
+--- util.lua.orig       2011-08-28 13:46:06.000000000 -0700
++++ util.lua    2011-08-28 13:46:11.000000000 -0700
+@@ -52,7 +52,7 @@
+        if str1 == nil or string.len(str1) == 0 then
+                return str2
+        else
+-               return str1 .. " " .. str2
++               return str1 .. "\n" .. str2
+        end
+ end
+
+I've suggested to the maintainers that they make this change.
+--]]
+
 standard = require("luadoc.doclet.html")
 lunamark = require("lunamark")
 
@@ -21,7 +36,6 @@ local mydoclet = standard
 
 function mydoclet.start(doc)
   markdownify(doc)
-  -- print_r(doc)
   return oldstart(doc)
 end
 
