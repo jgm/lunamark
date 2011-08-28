82523ab0cc1bb3e926d423d121b6aa48c9d5899f
John MacFarlane <fiddlosopher@gmail.com>
2011-08-27 17:40:21 -0700

    Added library documentation comment.
    We will create a program to parse these as markdown and
    produce documentation pages.

--- lunamark.lua
+++ lunamark.lua
@@ -1,6 +1,18 @@
 -- (c) 2009-2011 John MacFarlane.  Released under MIT license.
 -- See the file LICENSE in the source for details.
 
+--[[|
+Usage:
+
+    lunamark = require("lunamark")
+
+and then all the lunamark modules will be available.
+So, for example, one can refer to "lunamark.reader.markdown".
+Modules are not loaded until they are actually needed, so
+it is safe to require("lunamark") even if you only intend to
+use one reader and one writer.
+--]]
+
 local G = {}
 
 setmetatable(G,{ __index = function(t,name)
