8cbd51d98f0db05609a38626c204f5d60f02267e
John MacFarlane <fiddlosopher@gmail.com>
2011-08-29 23:32:42 -0700

    Updated lunadoc to act more like luadoc.
    - Comments start with ---
    - Function names automatically extracted.

--- lunadoc.lua
+++ lunadoc.lua
@@ -1,36 +1,59 @@
--- This program reads a number of source files, scans them
--- for multiline comment sections beginning with |,
--- and parses these as markdown, one big HTML file with sections
--- for each module.
+--- This program reads a number of source files, scans them
+-- for special comment blocks, and produces an HTML file with
+-- documentation for each module and the functions contained in it.
+--
+-- Lunadoc processes only comment blocks that start with '---'.
+--
+-- If the comment block immediately precedes a function definition,
+-- the name of the function is extracted automatically.
 
 lunamark = require("lunamark")
 
 local function extract_comments(f)
   local commentlines = {}
+  local chunks = {}
   local collect = false
+  local fun
   io.input(f)
   for l in io.lines() do
-    if l:sub(1,5) == "--[[|" then
+    local m = l:match("^%-%-%-%s?(.*)")
+    if m then
       collect = true
-    elseif l:sub(1,4) == "--]]" then
-      collect = false
-      table.insert(commentlines,"")
+      table.insert(commentlines,m)
     elseif collect then
-      table.insert(commentlines,l)
+      local n = l:match("^%-%-%s?(.*)")
+      if n then
+        table.insert(commentlines,n)
+      else
+        collect = false
+        fun = l:match("function [^%.]*%.([%a_]+%([^%)]*%))")
+        table.insert(chunks, { contents = table.concat(commentlines,"\n"), fun = fun })
+        commentlines = {}
+      end
     end
   end
-  return table.concat(commentlines,"\n")
+  return chunks
 end
 
 local converter = lunamark.reader.markdown.new(lunamark.writer.html.new(),{smart=true})
 
 for i=1,#arg do
   local f = arg[i]
-  local comments = extract_comments(f)
+  local chunks = extract_comments(f)
   local modname = f:gsub("%.lua$",""):gsub("/",".")
-  local inp = lunamark.util.get_input(comments)
-  io.write("<div id=\"" .. modname .. "\">\n<h1>" .. modname .. "</h1>\n")
-  io.write(converter(inp))
+  io.write("<div id=\"" .. modname .. "\">\n<h1 class=\"module\">" .. modname .. "</h1>\n")
+  for _,chunk in ipairs(chunks) do
+    local fun = chunk.fun
+    local funid = fun and fun:match("^[^%(]*")
+    local inp = lunamark.util.get_input(chunk.contents)
+    if fun then
+      io.write("<div id=\"" .. funid .. "\">\n<h2 class=\"function\">" .. chunk.fun .. "</h2>\n")
+    end
+    io.write(converter(inp))
+    if fun then
+      io.write("\n</div>\n")
+    end
+  end
   io.write("\n</div>\n\n")
 end
 
