1ea161d7999533c25cefd40a0f49f7cef6298eb2
John MacFarlane <fiddlosopher@gmail.com>
2011-08-30 20:33:33 -0700

    Improved lunadoc; it now writes a whole directory with index.

--- lunadoc.lua
+++ lunadoc.lua
@@ -1,13 +1,17 @@
 --- This program reads a number of source files, scans them
--- for special comment blocks, and produces an HTML file with
--- documentation for each module and the functions contained in it.
---
--- Lunadoc processes only comment blocks that start with '---'.
---
+-- for special comment blocks beginning with '---', and writes
+-- an HTML file documenting each module to the doc directory.
 -- If the comment block immediately precedes a function definition,
 -- the name of the function is extracted automatically.
+--
+-- Each file is assumed to be a module.  The 'module' keyword need
+-- not be used.
+--
+-- Documentation is parsed as markdown.
 
 lunamark = require("lunamark")
+
+local destdir = "doc"
 
 local function extract_comments(f)
   local commentlines = {}
@@ -35,26 +39,76 @@ local function extract_comments(f)
   return chunks
 end
 
-local converter = lunamark.reader.markdown.new(lunamark.writer.html.new(),{smart=true})
+local template = [[
+<html>
+<head>
+<title>${modname}</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="lunadoc.css" type="text/css" />
+</head>
+<body>
+<div id="content">
+<h1 class="module">${modname}</h1>
+${contents}
+</div>
+<div id="index">
+${index}
+</div>
+</body>
+</html>
+]]
+
+local funtemplate = [[
+<div id="${id}">
+<h2 class="function">${name}</h2>
+${contents}
+</div>
+]]
+
+local css = [[
+div#index { position: absolute; left: 1em; top: 1em; width:14em; border-right: 1px solid grey; }
+div#content { position: absolute; left: 15em; top: 0em; padding-left: 2em; max-width: 40em; }
+]]
+
+local index_table = {}
+
+local function get_modname(s)
+  return s:gsub("%.lua$",""):gsub("/",".")
+end
+
+for i=1,#arg do
+  local modname = get_modname(arg[i])
+  table.insert(index_table, "<p><a href=\"" .. modname .. ".html\">" .. modname .. "</a></p>")
+end
+
+local index = table.concat(index_table,"\n")
+
+local writer = lunamark.writer.html.new()
+writer.link = new_link
+local converter = lunamark.reader.markdown.new(writer,{smart=true})
 
 for i=1,#arg do
   local f = arg[i]
   local chunks = extract_comments(f)
-  local modname = f:gsub("%.lua$",""):gsub("/",".")
-  io.write("<div id=\"" .. modname .. "\">\n<h1 class=\"module\">" .. modname .. "</h1>\n")
+  local modname = get_modname(f)
+  local funs = {}
   for _,chunk in ipairs(chunks) do
     local fun = chunk.fun
     local funid = fun and fun:match("^[^%(]*")
     local inp = lunamark.util.get_input(chunk.contents)
     if fun then
-      io.write("<div id=\"" .. funid .. "\">\n<h2 class=\"function\">" .. chunk.fun .. "</h2>\n")
-    end
-    io.write(converter(inp))
-    if fun then
-      io.write("\n</div>\n")
+      local funtext = funtemplate:gsub("${(%w+)}", { name = fun, id = funid, contents = converter(inp) })
+      table.insert(funs, funtext)
+    else
+      table.insert(funs, converter(inp))
     end
   end
-  io.write("\n</div>\n\n")
+  local page = template:gsub("${(%w+)}",{ modname = modname, index = index, contents = table.concat(funs) })
+  local file = io.open(destdir .. "/" .. modname .. ".html", "w")
+  file:write(page)
+  file:close()
 end
 
-
+local file = io.open(destdir .. "/lunadoc.css", "w")
+file:write(css)
+file:close()
