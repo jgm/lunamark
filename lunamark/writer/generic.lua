5a58b70223543d3e65696cc2702db9a7ca75b800
John MacFarlane <fiddlosopher@gmail.com>
2011-09-02 19:50:58 -0700

    Added instances of all writers to generic writer.
    Partly this is to provide a place where they can all be documented.

--- lunamark/writer/generic.lua
+++ lunamark/writer/generic.lua
@@ -31,7 +31,97 @@ function M.new(options)
     return s
   end
 
-  return util.extend(W)
+  W.linebreak = "\n"
+
+  W.sep = { interblock = {compact = "\n", default = "\n\n", minimal = ""},
+            container = { compact = "\n", default = "\n", minimal = ""}
+          }
+
+  W.interblocksep = W.sep.interblock.default
+
+  W.containersep = W.sep.container.default
+
+  W.ellipsis = "…"
+
+  W.mdash = "—"
+
+  W.ndash = "–"
+
+  function W.singlequoted(s)
+    return string.format("‘%s’",s)
+  end
+
+  function W.doublequoted(s)
+    return string.format("“%s”",s)
+  end
+
+  function W.string(s)
+    return s
+  end
+
+  function W.code(s)
+    return s
+  end
+
+  function W.link(lab,src,tit)
+    return lab
+  end
+
+  function W.image(lab,src,tit)
+    return lab
+  end
+
+  function W.paragraph(s)
+    return s
+  end
+
+  function W.listitem(s)
+    return s
+  end
+
+  function W.bulletlist(s,tight)
+    return s
+  end
+
+  function W.orderedlist(s,tight,startnum)
+    return s
+  end
+
+  function W.inline_html(s)
+    return ""
+  end
+
+  function W.display_html(s)
+    return ""
+  end
+
+  function W.emphasis(s)
+    return s
+  end
+
+  function W.strong(s)
+    return s
+  end
+
+  function W.blockquote(s)
+    return s
+  end
+
+  function W.verbatim(s)
+    return s
+  end
+
+  function W.section(s,level,contents)
+    if contents then
+      return s .. W.interblocksep .. contents
+    else
+      return s
+    end
+  end
+
+  W.hrule = ""
+
+  return util.table_copy(W)
 end
 
 return M
