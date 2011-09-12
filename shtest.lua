f4ac46fcb67011cd278dfc42aa0bfa1e98f33af7
John MacFarlane <fiddlosopher@gmail.com>
2011-09-11 18:29:33 -0700

    Basics of shtest.lua now working.

--- shtest.lua
+++ shtest.lua
@@ -1,6 +1,7 @@
 local testdir = tests
 local lfs = require("lfs")
 local diff = require("diff")
+local cmdname = "lunamark"
 
 local function do_matching_tests(path, patt, fun)
   local patt = patt or "."
@@ -10,12 +11,49 @@ local function do_matching_tests(path, patt, fun)
     if f ~= "." and f ~= ".." and f:match(patt) and f:match("%.test$") then
       local fh = io.open(fpath, "r")
       local contents = fh:read("*all"):gsub("\r","")
-      local cmd, inp, out = contents:match("^([^\n]*)\n<<<[ \t]*\n(.-\n)>>>[ \t]*\n(.*)")
+      local cmd, inp, out = contents:match("^([^\n]*)\n<<<[ \t]*\n(.-\n)>>>[ \t]*\n(.*)$")
+      assert(cmd ~= nil, "Command not found in " .. f)
+      cmd = cmd:gsub("^(%S+)",cmdname)
       fun({ name = f:match("^(.*)%.test$"), path = fpath,
-            command = cmd, input = inp, output = out })
+            command = cmd, input = inp or "", output = out or ""})
+      fh:close()
     end
   end
 end
 
+local format = string.format
+
+local function show_diff(expected, actual)
+  local difflines = diff.diff(expected, actual,"\n")
+  for _,l in ipairs(difflines) do
+    local text, status = l[1], l[2]
+    if status == "in" then
+      io.write(format("+ %s\n",text))
+    elseif status == "out" then
+      io.write(format("- %s\n",text))
+    end
+  end
+end
+
+local function run_test(test)
+  local tmp = os.tmpname()
+  local tmph = io.open(tmp, "w")
+  tmph:write(test.input)
+  tmph:close()
+  local cmd = test.command .. " " .. tmp
+  local outh = io.popen(cmd, "r")
+  local actual_out = outh:read("*all")
+  outh:close()
+  os.remove(tmp)
+  io.write(test.name .. "...")
+  if actual_out == test.output then
+    io.write("PASS\n")
+  else
+    io.write("FAIL\n")
+    io.write(cmd .. "\n")
+    show_diff(test.output, actual_out)
+  end
+end
+
 -- test:
-do_matching_tests(arg[1], arg[2], function(t) print(t.name, t.command) end)
+do_matching_tests(arg[1], arg[2], run_test)
