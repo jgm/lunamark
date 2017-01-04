3fd4f8935a0a03ba2daef315ab8e9a4f0447387e
Vít Novotný <witiko@mail.muni.cz>
2017-01-05 00:36:24 +0100

    Fixed blockquote parsing.

--- lunamark/reader/markdown.lua
+++ lunamark/reader/markdown.lua
@@ -1050,7 +1050,8 @@ function M.new(writer, options)
   -- strip off leading > and indents, and run through blocks
   larsers.Blockquote  = Cs((((parsers.leader * parsers.more * parsers.space^-1)/""
                              * parsers.linechar^0 * parsers.newline)^1
-                            * (-parsers.blankline * parsers.linechar^1
+                            * (-(parsers.leader * parsers.more
+                                + parsers.blankline) * parsers.linechar^1
                               * parsers.newline)^0 * parsers.blankline^0
                            )^1) / parse_blocks / writer.blockquote
 
